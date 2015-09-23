d941d75362498f7e981c814e1ee07d8884d6c467
kohrVid <kohrVid@zoho.com>
2015-09-23 16:35:21 +0100

    Have add Bootstrap to the application

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'main#index'
+
+   get  'about' 	=>  'main#about'
+   get  'contact'	=>  'main#contact'
+   get  'faq'   	=>  'main#faq'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
